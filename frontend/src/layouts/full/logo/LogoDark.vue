ce449eef88e7abadfa1dac420c037043489aabe4
Alexandr T <tarasov.ae@nikamed-it.ru>
2026-03-28 22:09:13 +0300

    feat(frontend): SVG-логотип вместо mdi-bitcoin в лого и auth-страницах - LogoDark/LogoLight: заменён mdi-bitcoin на img favicon.svg - LoginPage/RegisterPage: заменён компонент Logo на img favicon.svg 64x64

--- frontend/src/layouts/full/logo/LogoDark.vue
+++ frontend/src/layouts/full/logo/LogoDark.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="logo">
     <RouterLink to="/" aria-label="Rebit P2P" class="text-decoration-none d-flex align-center">
-      <v-icon color="primary" size="28" class="mr-2">mdi-bitcoin</v-icon>
+      <img src="/favicon.svg" alt="Rebit" width="32" height="32" class="mr-2" />
       <span class="text-h5 font-weight-bold">Rebit P2P</span>
     </RouterLink>
   </div>
